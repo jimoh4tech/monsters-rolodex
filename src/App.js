1630b058f65fd534f346789e74e82b09b64d21a0
Abu-Abdillah1 <olamide14044@gmail.com>
2021-07-07 12:19:29 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, ReactDOM } from "react";
+import BroswerRouter from "react-router-dom/BroswerRouter";
 
 import { CardList } from "./components/card-list/card-list.components";
 import { SearchBox } from "./components/search-box/search-box.components";
 
 import "./App.css";
+
+ReactDOM.render(
+  <BroswerRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BroswerRouter>
+);
 
 class App extends Component {
   constructor() {
@@ -20,14 +27,13 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
-  handleChange = (e) => this.setState({ searchField: e.target.value })
+  handleChange = (e) => this.setState({ searchField: e.target.value });
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonster = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
     );
     return (
-      
       <div className="App">
         <h1>Monster Rolodex</h1>
         <SearchBox
